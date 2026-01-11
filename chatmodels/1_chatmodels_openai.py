1a707249884f7cb4d7c0458af967a7a14ae2ee5f
ashxvegeta <ashutoshroy453@gmail.com>
2026-01-11 15:40:24 +0530

    max_completion_tokens in models

--- chatmodels/1_chatmodels_openai.py
+++ chatmodels/1_chatmodels_openai.py
@@ -1,7 +1,9 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 load_dotenv()
-
+# max_completion_tokens = the maximum number of tokens the model is allowed to generate in the response
+# Tokens ≠ words (a token can be a word or part of a word)
+# This limits the length of the AI’s answer
 model = ChatOpenAI(model_name="gpt-4o-mini", temperature=0,max_completion_tokens=10)
 result = model.invoke("Explain the causes of World War II?")
 print(result.content)
