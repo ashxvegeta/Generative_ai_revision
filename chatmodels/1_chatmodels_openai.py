d6652fac46059fc72e6208f9ffa2842c081571af
ashxvegeta <ashutoshroy453@gmail.com>
2026-01-11 15:38:28 +0530

    max_completion_tokens in models

--- chatmodels/1_chatmodels_openai.py
+++ chatmodels/1_chatmodels_openai.py
@@ -1,6 +1,7 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 load_dotenv()
-model = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
-result = model.invoke("what is the capital of France?")
+
+model = ChatOpenAI(model_name="gpt-4o-mini", temperature=0,max_completion_tokens=10)
+result = model.invoke("Explain the causes of World War II?")
 print(result.content)
