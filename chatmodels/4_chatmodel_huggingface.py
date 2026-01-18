584becb9fbec720bc0b054dabc8e9bb0ce698f96
ashxvegeta <ashutoshroy453@gmail.com>
2026-01-18 15:27:39 +0530

    chat Huggiang face mdoel code

--- chatmodels/4_chatmodel_huggingface.py
+++ chatmodels/4_chatmodel_huggingface.py
@@ -1,12 +1,14 @@
 from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
 from dotenv import load_dotenv
 load_dotenv()
+
 llm = HuggingFaceEndpoint(
-    repo_id= "HuggingFaceH4/zephyr-7b-beta",
+    repo_id="HuggingFaceH4/zephyr-7b-beta",
     task="text-generation",
     temperature=0.7,
-    max_new_tokens=50
+    max_new_tokens=256,
 )
-model = ChatHuggingFace(llm=llm)
-result = model.invoke("What is the capital of Germany?")
-print(result.content)
+
+model =ChatHuggingFace(llm=llm)
+response = model.invoke("Explain the theory of relativity in simple terms.")
+print(response.content)
